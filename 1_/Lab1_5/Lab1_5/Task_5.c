10426677f57c283e60fb7a65a8d99fdf57f6239a
Dmitriy Semenov <45949611+Semetory@users.noreply.github.com>
2023-09-29 23:19:23 +0300

    d

---
+++ 1_/Lab1_5/Lab1_5/Task_5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void house() {
+	puts("\t\t\t\t_________________________");
+	puts("\t\t\t\t|\t\t\t|");
+	puts("\t\t\t\t|\t\t\t|");
+	puts("\t\t\t\t|\t\t\t|");
+	puts("\t\t\t\t|\t_________\t|");
+	puts("\t\t\t\t|\t|\t|\t|________________________________");
+	puts("\t\t\t\t|\t|\t|\t|\t\t\t\t|");
+	puts("\t\t\t\t|\t|\t|\t|\t\t\t\t|");
+	puts("\t\t\t\t|\t|\t|\t|\t\t\t\t|");
+	puts("\t\t\t\t|\t|_______|\t|\t\t\t\t|");
+	puts("\t\t\t\t|\t\t\t|\t\t_________\t|");
+	puts("\t\t\t\t|\t\t\t|\t\t|\t|\t|");
+	puts("\t\t\t\t|\t\t\t|\t\t|\t|\t|");
+	puts("\t\t\t\t|\t\t\t|\t\t|\t|\t|");
+	puts("\t\t\t\t|\t\t\t|\t\t|\t|\t|");
+	puts("\t\t\t\t|\t\t\t|\t\t|\t|\t|");
+	puts("\t\t\t\t|\t\t\t|\t\t|\t|\t|");
+	puts("\t\t\t\t|\t\t\t|\t\t|\t|\t|");
+	puts("\t\t\t\t|_______________________|_______________|_______|_______|");
+
+
+}
+
+
+main() {
+	house();
+}
